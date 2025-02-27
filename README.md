5b9b314ba59068838997fe2ea99623b60b07bc24
dhiegoferreira <dev.dhiegoferreira@gmail.com>
2025-02-27 11:17:43 +0000

    update deploy tests

---
+++ README.md
@@ -0,0 +1,17 @@
+# Run Notes-BackEnd
+
+
+- Cors
+- Epress
+- Nodenom
+
+- Backend load dist files trough "serving static files" in express 
+
+"express.static(root, [options])"
+
+
+
+![alt text](/part3/resources/setup-deployment-notes-app.png)
+
+
+Unlike when running the app in a development environment, everything is now in the same node/express-backend that runs in localhost:3001.
--- index.js
+++ index.js
@@ -5,70 +5,69 @@ const cors = require('cors')
 
 
 app.use(cors())
-
 app.use(express.json()) //json-parser
+app.use(express.static('dist'))
 
-// const http = require('http')
 
+//Mock
 let notes = [
     {
-        id:"1",
+        id: "1",
         content: "HTML is easy",
         important: true
     },
     {
-        id:"2",
+        id: "2",
         content: "Browser can execute only Js",
         important: true
     },
     {
-        id:"3",
+        id: "3",
         content: "GET and POST are the most important methods of HTTP protocol.",
         important: true
     },
 ]
 
-app.get('/',(request, response) => {
+
+//To test endpoint
+app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
 })
 
 
-app.get('/api/notes',(request, response) => {
+app.get('/api/notes', (request, response) => {
     response.json(notes)
 })
 
 
-app.get('/api/notes/:id',(request,response) =>{
+app.get('/api/notes/:id', (request, response) => {
     const id = request.params.id
-    const note = notes.find(note => note.id ===id)
+    const note = notes.find(note => note.id === id)
 
     note ? response.json(note) : response.status(404).end()
 
-    
+
 })
 
 
-
-app.delete('/api/notes/:id',(request, response) => {
+app.delete('/api/notes/:id', (request, response) => {
 
     const id = request.params.id
 
     notes = notes.filter(note => note.id !== id)
-    
-
     response.status(204).end()
 })
 
 
 
 
-app.post('/api/notes',(request, response) =>{
-    
+app.post('/api/notes', (request, response) => {
+
     const body = request.body
 
 
-    if(!body.content){
-        return  response.status(400).json({
+    if (!body.content) {
+        return response.status(400).json({
             error: 'content missing'
         })
     }
@@ -83,23 +82,45 @@ app.post('/api/notes',(request, response) =>{
 
 
     notes = notes.concat(note)
-    
-    
+
+
     response.json(note)
 })
 
 
 const generateId = () => {
-    
+
     const maxId = notes.length > 0 ? Math.max(...notes.map(n => Number(n.id))) : 0
 
     return String(maxId + 1)
 }
 
+app.put('/api/notes/:id', (request, response) => {
+
+    const id = request.params.id
+
+    console.log(id)
+    //save note
+    noteToUpdate = notes.find(note => note.id === id)
+   
+    console.log(noteToUpdate.important)
+
+    const newNote = {
+        content: noteToUpdate.content,
+        important: !(noteToUpdate.important),
+        id: noteToUpdate.id
+    }
+
+    console.log(newNote.important)
+
+    response.json(newNote)
+
+
+})
 
 
 
-const PORT = process.env.PORT || 3001
+const PORT = 3001
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
